24bb4f4f26d99ceb58fcf9e356390cc219b67cc5
marcin <s12497@pjwstk.edu.pl>
2023-06-13 19:49:30 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { createElement } from "react"
+
+function App() {
+
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+        {id: 4, text: 'Comment four'},
+        {id: 5, text: 'Comment five'}
+    ]
+
+    return (
+        <div className="container">
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default App
