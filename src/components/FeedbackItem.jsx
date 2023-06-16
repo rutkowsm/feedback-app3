9d2614013d41e48941fd0ee6d9583ce94690e0c3
marcin <s12497@pjwstk.edu.pl>
2023-06-16 17:10:48 +0200

    Deleting feedbacks + count of reviews + average

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { useState } from 'react' 
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+import {FaTimes} from 'react-icons/fa'
 
-function FeedbackItem() {
-
-    const [rating, setRating] = useState(7) 
-    const [text, setText] = useState('Example of a feedback item') 
+function FeedbackItem({item, handleDelete}) {
 
     return (
-        <div className='card'>
-            <div className="num-display">{rating}</div>
-            <div className="text-display">Example of a feedback item</div>
-        </div>
+        <Card>
+            <div className="num-display">{item.rating}</div>
+            <button onClick={() => handleDelete(item.id)} className='close'>
+                <FaTimes color='purple'/>
+            </button>
+            <div className="text-display">{item.text}</div>
+        </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
