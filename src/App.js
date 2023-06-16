ac4b3bb3e6d2469e2ac299968174275d67448945
marcin <s12497@pjwstk.edu.pl>
2023-06-17 01:19:27 +0200

    Adjust to Router v6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from "react"
 import React, { createElement } from "react"
 import Header from "./components/Header"
@@ -6,6 +7,7 @@ import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import About from './pages/About'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -23,15 +25,24 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header />
-            <div className="container">
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback}
-                handleDelete={deleteFeedback}/>
+            <div className='container'>
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackStats feedback={feedback}/>
+                            <FeedbackList feedback={feedback}
+                            handleDelete={deleteFeedback}/>
+                        </>
+                    }>
+                        
+                    </Route>
+                    <Route path='/about' element={<About />}/>
+                </Routes>
             </div>
-        </>
+        </Router>
     )
 }
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../components/shared/Card'
+
+function About() {
+  return <Card>
+    <div className="about">
+        <h1>About this project</h1>
+        <p>This is an app to give feedback</p>
+    </div>
+  </Card>
+}
+
+export default About
